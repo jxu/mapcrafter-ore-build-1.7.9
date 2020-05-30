f6470e22c9d275902e69d757595ec5069143e170
jxu <7989982+jxu@users.noreply.github.com>
2020-05-30 15:57:37 -0400

    Initial commit

---
+++ config.js
@@ -0,0 +1,108 @@
+var CONFIG = {
+  "maps": {
+    "build_1.7.9_iso": {
+      "defaultView": [
+        0,
+        0,
+        64
+      ],
+      "defaultZoom": 4,
+      "imageFormat": "png",
+      "lastRendered": [
+        1590794460,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 7,
+      "name": "Build 1.7.9 isometric",
+      "renderView": "isometric",
+      "rotations": [
+        0
+      ],
+      "textureSize": 4,
+      "tileSetGroup": "build_1.7.9_isometric_t4",
+      "tileSize": 512,
+      "world": "build_1.7.9",
+      "worldName": "world",
+      "worldSeaLevel": 64
+    },
+    "build_1.7.9_top": {
+      "defaultView": [
+        0,
+        0,
+        64
+      ],
+      "defaultZoom": 4,
+      "imageFormat": "png",
+      "lastRendered": [
+        1590794460,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 7,
+      "name": "Build 1.7.9 top-down",
+      "renderView": "topdown",
+      "rotations": [
+        0
+      ],
+      "textureSize": 4,
+      "tileSetGroup": "build_1.7.9_topdown_t4",
+      "tileSize": 256,
+      "world": "build_1.7.9",
+      "worldName": "world",
+      "worldSeaLevel": 64
+    }
+  },
+  "mapsOrder": [
+    "build_1.7.9_top",
+    "build_1.7.9_iso"
+  ],
+  "tileSetGroups": {
+    "build_1.7.9_isometric_t4": {
+      "maxZoom": 7,
+      "tileOffsets": [
+        [
+          -1,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 4
+    },
+    "build_1.7.9_topdown_t4": {
+      "maxZoom": 7,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 4
+    }
+  }
+};
